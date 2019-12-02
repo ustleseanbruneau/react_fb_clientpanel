1e0b80995a5d93dcbaeb52370698f8e25cfcde66
MN JS Dude <ustbrun9912@gmail.com>
2019-12-01 19:07:32 -0600

    Section 56 - Setup & AppNavbar Components

---
+++ src/components/layout/AppNavbar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+//import { exportNamedDeclaration } from '@babel/types'
+
+class AppNavbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4" >
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            ClientPanel
+          </Link>
+          <button className="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarMain">
+            <span className="navbar-toggle-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarMain">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+}
+
+export default AppNavbar;
