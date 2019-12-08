7a01e0c021bcd4569bbf2ce036cdfde1f8424d4b
MN JS Dude <ustbrun9912@gmail.com>
2019-12-08 07:32:39 -0600

    Section 70 - Persist Settings To LocalStorage

--- src/store.js
+++ src/store.js
@@ -46,8 +46,22 @@ const rootReducer = combineReducers({
   settings: settingsReducer
 });
 
+// Check for settings in localStorage
+if(localStorage.getItem('settings') == null) {
+  //Default setings
+  const defaultSettings = {
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: false,
+    allowRegistration: false
+  } 
+
+  // Set to local storage
+  localStorage.setItem('settings', JSON.stringify(defaultSettings));
+
+}
+
 // Create initial state
-const initialState = {};
+const initialState = {settings: JSON.parse(localStorage.getItem('settings'))};
 
 //Create store
 const store = createStore(
